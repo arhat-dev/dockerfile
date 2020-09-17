ddde937aaa8b25f1235ae88e17d95cf7a0de0cc9
Jeffrey Stoke <me@arhat.dev>
2020-09-17 13:28:15 +0200

    Fix download script

--- scripts/versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-QEMU_VERSION="v5.1.0-2"           # https://github.com/multiarch/qemu-user-static/releases
-V2RAY_VERSION="v4.27.5"           # https://github.com/v2ray/v2ray-core/releases
-FRP_VERSION="0.33.0"              # https://github.com/fatedier/frp/releases
-GITHUB_RUNNER_VERSION="v2.273.0"  # https://github.com/actions/runner/releases
-KUBECTL_VERSION="v1.18.8"         # https://github.com/kubernetes/kubernetes/releases
-HELM_VERSION="v3.3.1"             # https://github.com/helm/helm/releases
