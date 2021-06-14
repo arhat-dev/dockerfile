c707641ebd41001d5d5b0b6b6db38d742e907e90
Jeffrey Stoke <me@arhat.dev>
2021-06-14 19:04:45 +0200

    ci: Disable buildkit, update build tools
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- scripts/app/app.mk
+++ scripts/app/app.mk
@@ -5,7 +5,7 @@ GOPATH := $(shell go env GOPATH)
 .app-build-dir:
 	mkdir -p /app/build
 
-HYDROXIDE_PKG := github.com/emersion/hydroxide/cmd/hydroxide@v0.2.15
+HYDROXIDE_PKG := github.com/emersion/hydroxide/cmd/hydroxide@v0.2.18
 hydroxide-linux-amd64: .app-build-dir
 	$(GO_ARGS) GOARCH=amd64 go get $(HYDROXIDE_PKG)
 	find $(GOPATH)/bin -name 'hydroxide' -exec mv {} /app/build/$@ \;
@@ -23,7 +23,7 @@ hydroxide-linux-arm64: .app-build-dir
 	find $(GOPATH)/bin -name 'hydroxide' -exec mv {} /app/build/$@ \;
 
 
-KUBEVAL_PKG := github.com/instrumenta/kubeval@0.15.0
+KUBEVAL_PKG := github.com/instrumenta/kubeval@0.16.3
 kubeval-linux-amd64: .app-build-dir
 	$(GO_ARGS) GOARCH=amd64 go get $(KUBEVAL_PKG)
 	find $(GOPATH)/bin -name 'kubeval' -exec mv {} /app/build/$@ \;
