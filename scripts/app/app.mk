b2b7a95c5ea4cd4c1114847fc91c0d4acd5561c9
Jeffrey Stoke <me@arhat.dev>
2021-06-15 05:55:22 +0200

    app: Remove hydroxide
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- scripts/app/app.mk
+++ scripts/app/app.mk
@@ -5,25 +5,7 @@ GOPATH := $(shell go env GOPATH)
 .app-build-dir:
 	mkdir -p /app/build
 
-HYDROXIDE_PKG := github.com/emersion/hydroxide/cmd/hydroxide@v0.2.18
-hydroxide-linux-amd64: .app-build-dir
-	$(GO_ARGS) GOARCH=amd64 go get $(HYDROXIDE_PKG)
-	find $(GOPATH)/bin -name 'hydroxide' -exec mv {} /app/build/$@ \;
-
-hydroxide-linux-armv6: .app-build-dir
-	$(GO_ARGS) GOARCH=arm GOARM=6 go get $(HYDROXIDE_PKG)
-	find $(GOPATH)/bin -name 'hydroxide' -exec mv {} /app/build/$@ \;
-
-hydroxide-linux-armv7: .app-build-dir
-	$(GO_ARGS) GOARCH=arm GOARM=7 go get $(HYDROXIDE_PKG)
-	find $(GOPATH)/bin -name 'hydroxide' -exec mv {} /app/build/$@ \;
-
-hydroxide-linux-arm64: .app-build-dir
-	$(GO_ARGS) GOARCH=arm64 go get $(HYDROXIDE_PKG)
-	find $(GOPATH)/bin -name 'hydroxide' -exec mv {} /app/build/$@ \;
-
-
-KUBEVAL_PKG := github.com/instrumenta/kubeval@v0.16.3
+KUBEVAL_PKG := github.com/instrumenta/kubeval@v0.16.1
 kubeval-linux-amd64: .app-build-dir
 	$(GO_ARGS) GOARCH=amd64 go get $(KUBEVAL_PKG)
 	find $(GOPATH)/bin -name 'kubeval' -exec mv {} /app/build/$@ \;
