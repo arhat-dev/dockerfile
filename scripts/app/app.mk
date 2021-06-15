8fb94be0bcbc1f79dc49f344162b56343444b150
Jeffrey Stoke <me@arhat.dev>
2021-06-15 21:29:34 +0200

    app(proton-bridge): Return err for gnome keyring
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- scripts/app/app.mk
+++ scripts/app/app.mk
@@ -34,8 +34,8 @@ PROTON_BRIDGE_VERSION := v1.8.5
 .download-proton-bridge-src:
 	git clone --branch "${PROTON_BRIDGE_VERSION}" \
 		https://github.com/ProtonMail/proton-bridge.git
-	sed -i '/github.com\/docker\/docker-credential-helpers\/secretservice/d' proton-bridge/pkg/keychain/helper_linux.go 
-	sed -i 's/return \&secretservice\.Secretservice.*, nil/return nil, nil/g' proton-bridge/pkg/keychain/helper_linux.go
+	sed -i 's/github.com\/docker\/docker-credential-helpers\/secretservice/fmt/g' proton-bridge/pkg/keychain/helper_linux.go
+	sed -i 's/return \&secretservice\.Secretservice.*, nil/return nil, fmt\.Errorf\("not supported"\)/g' proton-bridge/pkg/keychain/helper_linux.go
 
 MK_PROTON_BRIDGE := CGO_ENABELD=0 GOOS=linux BUILD_TAGS='netgo' make -C proton-bridge build-nogui
 proton-bridge-linux-amd64: .app-build-dir .download-proton-bridge-src
